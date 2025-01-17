1294f9b4c131ef3e1d23c523591bde82564e1ba1
Potato-29 <csaprayas@gmail.com>
2025-01-17 18:04:37 +0530

    feat: handle duplicate hash case :rocket:

--- internal/api/handlers/handler.go
+++ internal/api/handlers/handler.go
@@ -2,10 +2,13 @@ package handlers
 
 import (
 	"api/url-shorter/internal/api/services"
+	"api/url-shorter/internal/db"
 	"api/url-shorter/internal/pkg/utils"
+	"context"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
 )
 
 func GetTestHandler(c *gin.Context) {
@@ -20,14 +23,20 @@ func ShortenUrl(c *gin.Context) {
 	var input struct {
 		BaseUrl string `json:"base-url"`
 	}
+	var duplicate services.UrlHash
 	if err := c.ShouldBindJSON(&input); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid input"})
-		return
 	}
 
 	urlHash, hashErr := utils.GenerateRandomBase64Hash(8)
 	if hashErr != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to generate short URL"})
+	}
+
+	err := db.GetCollection("url-hashes").FindOne(context.TODO(), bson.M{"hash": urlHash}).Decode(&duplicate)
+
+	if err == nil {
+		ShortenUrl(c)
 	}
 
 	result, err := services.InsertUrlDocument(input.BaseUrl, urlHash)
